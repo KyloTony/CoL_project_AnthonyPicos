60b3036b340c3b0bfe937cf9100cfb86fedc6765
KyloTony <89665745+KyloTony@users.noreply.github.com>
2021-10-28 17:11:18 -0400

    Deleted body code(remake), created responsive nav & js code for nav

---
+++ Scripts/script.js
@@ -0,0 +1,8 @@
+
+// Hamburger Menu drop down function //
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('nav-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
