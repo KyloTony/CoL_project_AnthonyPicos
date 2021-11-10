4629d7211a7be4cf592b44614bd03b413fded3c7
KyloTony <89665745+KyloTony@users.noreply.github.com>
2021-11-10 16:54:06 -0500

    Added modified files done on my secondary copy, (same project, work done on my laptop added now to main PC repo)

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,3 +6,4 @@ const navbarLinks = document.getElementsByClassName('nav-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+//---------------------------------//
